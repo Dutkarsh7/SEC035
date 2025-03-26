e04a065c9cc3440ec5d7f0a4ff1a657fd212fac8
Dutkarsh7 <utkarshshukla733@gmail.com>
2025-03-26 11:57:02 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,13 @@
+for i in range(5):
+    print(i)
+num = int(input("Enter a number: "))
+if num > 0:
+    print("Positive number")
+elif num < 0:
+    print("Negative number")
+else:
+    print("Zero")
+for i in range(5):
+    print(i)
+for i in range(5):
+    print(i)
